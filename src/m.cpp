af4ff1cbfa164c3e2b57a0061d2e1fc7f4285dab
dlb <dlb96@qq.com>
2017-09-20 16:49:34 +0800

    scale function test

--- src/m.cpp
+++ src/m.cpp
@@ -12,11 +12,17 @@ public:
     twoDimArray();
     twoDimArray(int,int);
     ~twoDimArray();
+    const getWidth(){
+        return this->width;
+    }
+    const getHeight() {
+        return this->height;
+    }
     int* operator[](int i);
     void print();
 };
 
-
+Tarray& scale(Tarray src, int height, int width);
 
 int main() {
     Tarray a(3,3);
@@ -32,13 +38,45 @@ int main() {
 
 
 
-twoDimArray::twoDimArray(int w,int h) {
-    this->width = w;
-    this->height = h;
-    this->a = createTwoDimArray(this->width, this->height);
+Tarray& scale(Tarray src, int height, int width) {
+    //计算放大倍数
+    //float scaleH = height / src.getHeight();
+    //float scaleW = width / src.getWidth();
+    float scaleH = height - 1 / src.getHeight() - 1;
+    float scaleW = width - 1 / src.getWidth() - 1;
+    
+    //创建新二维数组
+    Tarray* dst = new Tarray(height, width);
+    //遍历新数组进行赋值
+    for (int i = 0; i < height; i++) {
+        for (int j = 0; j < width; j++) {
+            //i行j列的像素
+            dst[i][j];
+            int X,Y;
+            float srcX, srcY, u,v;
+            //求出对应的虚拟像素
+            srcX = (float)j / scale;
+            srcY = (float)i / scaleH;
+            X = srcX;
+            Y = srcY;
+            u = srcX - X;
+            v = srcY - Y;
+            //通过(X,Y),(X,Y+1),(X+1,Y),(X+1,Y+1)四点确定目标点像素
+            dst[i][j] = src[X][Y] * (1-u) * (1-v) + 
+                        src[X+1][Y] * u * (1-v) +
+                        src[X][Y+1] * (1-u) * v +
+                        src[X+1][Y+1] * u * v;
+        }
+    }
 }
 
-int** twoDimArray::createTwoDimArray(int width, int height) {
+twoDimArray::twoDimArray(int height,int width) {
+    this->width = width;
+    this->height = height;
+    this->a = createTwoDimArray(this->height, this->width);
+}
+
+int** twoDimArray::createTwoDimArray(int height, int width) {
     int **a;
     a = new int* [height];
     for (int i = 0; i < height; i++) {
